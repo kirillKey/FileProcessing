5f4860e154e82c55e42bfd97a6733a9f86cfcdf0
Kirill Ushkov <kirillushkov2010@gmail.com>
2015-04-29 19:02:23 +0300

    fixed reading file

--- FileProcessing/FileStreamParser.m
+++ FileProcessing/FileStreamParser.m
@@ -42,11 +42,16 @@ static const NSInteger maxBufLength = 512;
     iStream = [[NSInputStream alloc] initWithFileAtPath:path];
     [iStream setDelegate:self];
     
-    dispatch_async(dispatch_get_main_queue(), ^{
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+    
+    dispatch_async(queue, ^{
         [iStream scheduleInRunLoop:[NSRunLoop currentRunLoop]
                            forMode:NSDefaultRunLoopMode];
         [iStream open];
+        
+        [[NSRunLoop currentRunLoop] run];
     });
+    
 }
 
 #pragma mark - NSStreamdelegate - 
--- FileProcessing/main.m
+++ FileProcessing/main.m
@@ -15,25 +15,17 @@ int main(int argc, const char * argv[]) {
         NSBundle *bundle = [NSBundle bundleWithURL:bundleURL];
         NSString *path = [bundle pathForResource:@"file" ofType:@"txt"];
 
-        dispatch_queue_t q = dispatch_queue_create("fileprocessingqueue", NULL);
-        
         dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-        dispatch_async(dispatch_get_main_queue(), ^{
-            FileStreamParser *parser = [[FileStreamParser alloc] initWithPath:path
-                                                          withProcessingBlock:^(NSString *string) {
-                                                              NSLog(@"string %@", string);
-                                                          }
-                                                          withCompletionBlock:^{
-                                                              NSLog(@"completion");
-                                                              dispatch_semaphore_signal(semaphore);
-                                                          }];
+        FileStreamParser *parser = [[FileStreamParser alloc] initWithPath:path
+                                                      withProcessingBlock:^(NSString *string) {
+                                                          NSLog(@"string %@", string);
+                                                      }
+                                                      withCompletionBlock:^{
+                                                          NSLog(@"completion");
+                                                          dispatch_semaphore_signal(semaphore);
+                                                      }];
+        [[NSRunLoop currentRunLoop] run];
 
-        });
-        
-        while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW)) {
-            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
-        }
-        
         dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
     }
     return 0;
